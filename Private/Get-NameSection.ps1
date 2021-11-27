0e452d4228f3c03dabfd0a623be5b28363ebacac
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-27 18:21:54 +0100

    First draft

---
+++ Private/Get-NameSection.ps1
@@ -0,0 +1,31 @@
+﻿Function Get-NameSection {
+    <#
+    .SYNOPSIS
+    Returns the first $num characters of a name, unless $num is 0, missing or longer than the name, then returns the entire name
+
+    .DESCRIPTION
+    Returns the first $num characters of a name, unless $num is 0, missing or longer than the name, then returns the entire name
+
+    .PARAMETER Name
+    Parameter description
+
+    .PARAMETER Number
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding()]
+    param(
+        [string]$Name,
+        [int]$Number
+    )
+    if (-not $Number -or $Number -gt $Name.Length) {
+        $Name
+    } else {
+        $Name.Substring(0, $Number)
+    }
+}
---
+++ Public/Get-EmailAddressPolicy.ps1
@@ -0,0 +1,28 @@
+﻿function Get-EmailAddressPolicy {
+    [cmdletBinding()]
+    param(
+        [string] $FirstName,
+        [string] $LastName,
+        [string[]] $EmailTemplate,
+        [alias('LowerCase')][switch] $ToLowerCase
+    )
+
+    $FirstName = Rename-LatinCharacters -String $FirstName
+    $LastName = Rename-LatinCharacters -String $LastName
+    foreach ($Template in $EmailTemplate) {
+        $FullEmail = $Template
+        $FullEmail = [regex]::Replace($FullEmail, '%(\d*)g', { param($m) Get-NameSection -Name $FirstName -Number $m.Groups[1].Value })
+        if ($LastName) {
+            # Last Name exists so we do standard approach
+            $FullEmail = [regex]::Replace($FullEmail, '%(\d*)s', { param($m) Get-NameSection -Name $LastName -Number $m.Groups[1].Value })
+        } else {
+            # Last Name doesn't exists so we remove everything including dot
+            $FullEmail = [regex]::Replace($FullEmail, '.%(\d*)s', { param($m) Get-NameSection -Name $LastName -Number $m.Groups[1].Value })
+        }
+        if ($ToLowerCase) {
+            $FullEmail.ToLower()
+        } else {
+            $FullEmail
+        }
+    }
+}
