539dc47e0e3c3547a2f7bcdb3d1c1a8c21afaf9e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-27 19:44:43 +0100

    Update examples

---
+++ Examples/Example-EmailAddressPolicy01.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\O365EmailAddressPolicy.psd1 -Force
+
+$EmailTemplate = @(
+    "%g.%s@evotec.xyz"  # would give przemyslaw.klys@evotec.xyz
+    "%1g.%s@evotec.xyz" # would give p.klys@evotec.xyz
+    "%1g%s@evotec.xyz"  # would give pklys@evotec.xyz
+    "%3g.%s@evotec.xyz" # would give prz.klys@evotec.xyz
+    "%g.%1s@evotec.xyz" # would give przemyslaw.k@evotec.xyz
+    "%g@company.pl"         # would give przemyslaw@company.pl
+)
+
+Get-EmailAddressPolicy -FirstName 'Przemysław' -LastName 'Kłys' -EmailTemplate $EmailTemplate -ToLower
+#Get-EmailAddressPolicy -FirstName 'Adam' -LastName '' -EmailTemplate $EmailTemplate -ToLower
+#Get-EmailAddressPolicy -LastName 'Kolendo' -EmailTemplate $EmailTemplate -ToLower
---
+++ Examples/Example-EmailAddressPolicy02.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\O365EmailAddressPolicy.psd1 -Force
+
+$EmailTemplate = @(
+    "%g%s@evotec.xyz"  # would give przemyslawklys@evotec.xyz
+)
+
+Get-EmailAddressPolicy -FirstName 'Przemysław' -LastName 'Kłys' -EmailTemplate $EmailTemplate -ToLower
